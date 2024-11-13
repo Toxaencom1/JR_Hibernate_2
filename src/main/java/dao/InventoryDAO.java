f2b113e2a1bfc98c95894c0d6a1bcc20bcf1b54f
Toxaencom1 <Toxaencom@gmail.com>
2024-11-13 11:36:00 +0500

    fixed pickAvailableInventoryMovie behavior

--- src/main/java/dao/InventoryDAO.java
+++ src/main/java/dao/InventoryDAO.java
@@ -14,23 +14,17 @@ public class InventoryDAO extends BaseDAO<Inventory> {
 
     public Inventory pickAvailableInventoryMovie(int filmId) {
         Query<Rental> query1 = sessionFactory.getCurrentSession()
-                .createQuery("select r from Rental r left join r.inventory i where i.film.id = :filmId AND r.returnDate IS NOT NULL", Rental.class);
+                .createQuery("select r from Rental r left join r.inventory i where i.film.id = :filmId", Rental.class);
         query1.setParameter("filmId", filmId);
-        List<Rental> resultListNotNull = query1.getResultList();
+        List<Rental> resultList = query1.getResultList();
+        List<Inventory> notNullList = resultList.stream().filter(rental -> rental.getReturnDate()!=null).map(Rental::getInventory).toList();
+        List<Inventory> nullList = resultList.stream().filter(rental -> rental.getReturnDate()==null).map(Rental::getInventory).toList();
 
-        Query<Rental> query2 = sessionFactory.getCurrentSession()
-                .createQuery("select r from Rental r left join r.inventory i where i.film.id = :filmId AND r.returnDate IS NULL", Rental.class);
-        query2.setParameter("filmId", filmId);
-        List<Rental> resultListNull = query2.getResultList();
-
-        List<Inventory> inventoryListNotNull = resultListNotNull.stream().map(Rental::getInventory).toList();
-
-        if (resultListNull.isEmpty()){
-            return inventoryListNotNull.get(0);
-        } else if (!resultListNotNull.isEmpty()) {
-            List<Inventory> inventoryListNull = resultListNull.stream().map(Rental::getInventory).toList();
-            for (Inventory inventory : inventoryListNotNull) {
-                if (!inventoryListNull.contains(inventory)) {
+        if (nullList.isEmpty()){
+            return notNullList.get(0);
+        } else if (!notNullList.isEmpty()) {
+            for (Inventory inventory : notNullList) {
+                if (!nullList.contains(inventory)) {
                     return inventory;
                 }
             }
